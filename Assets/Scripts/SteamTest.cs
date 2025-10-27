fc7eb10947f3bd878a02f5261c30e0aa1c01ffc3
Rajan332002 <infotechsrealm.rajan@gmail.com>
2025-10-27 19:31:23 +0530

    Fisherman Achievements  ● Fish Slayer — Catch the maximum number of fish in one round.  Fish Achievements  ● What a Snack — Eat the Gold Trout.  ● Survivor — Escape all hooks and survive the round.  ● We Come in Swarms — Full 7-player lobby where all fish survive.  ● Solo Artist — Win as the last fish standing.  ● Gulper — Eat 30 worms in one round.

---
+++ Assets/Scripts/SteamTest.cs
@@ -0,0 +1,27 @@
+﻿using Steamworks;
+using UnityEngine;
+
+public class SteamTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        if (SteamAPI.Init())
+        {
+            Debug.Log("✅ Steam initialized successfully!");
+            Debug.Log("🎮 Steam Name: " + SteamFriends.GetPersonaName());
+            int avatarId = SteamFriends.GetSmallFriendAvatar(SteamUser.GetSteamID());
+        }
+        else
+        {
+            Debug.LogError("❌ Steam initialization failed!");
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
